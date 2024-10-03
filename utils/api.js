b2341d2c398eec3cf1b8c17ae0d15974c691e1e1
Don Stevenson <50342473+Don-Stevenson@users.noreply.github.com>
2024-10-03 16:34:13 -0400

    Update base URL (#8)

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5050/api/',
+  baseURL: 'https://loons-team-balancer.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },

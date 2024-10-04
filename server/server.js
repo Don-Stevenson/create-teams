78bce008347ab2eb1b480c189b7861db346699c7
Don Stevenson <50342473+Don-Stevenson@users.noreply.github.com>
2024-10-03 21:22:22 -0400

    Client update base url (#9)
    * Update base URL
    
    * - update URLS to use deployment urls

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 app.use(helmet())
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://create-teams.vercel.app',
     credentials: true,
   })
 )

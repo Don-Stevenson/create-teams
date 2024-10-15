8878373c77ec81b4944ccd562a46381c1a889de8
Don Stevenson <dstevenson@tribalscale.com>
2024-10-15 12:43:26 -0400

    Handle merge conflict

--- utils/api.js
+++ utils/api.js
@@ -3,11 +3,7 @@ import axios from 'axios'
 import config_url from '../config'
 
 const api = axios.create({
-<<<<<<< HEAD
   baseURL: `${config_url}/api/`,
-=======
-  baseURL: 'http://localhost:5050/api/',
->>>>>>> d390735 (WIP: add select all / remove all for playing this week)
   headers: {
     'Content-Type': 'application/json',
   },

313e7ddcfc37e79cda886e0fefbb5b1649491069
Don Stevenson <dstevenson@tribalscale.com>
2024-10-18 11:42:27 -0400

    Add login page unit test

---
+++ __tests__/pages/login.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Login from '../../pages/login'
+import LoginForm from '../../src/app/components/LoginForm'
+
+jest.mock('../../src/app/components/LoginForm', () => {
+  return jest.fn(() => (
+    <div data-testid="mocked-login-form">Mocked Login Form</div>
+  ))
+})
+
+describe('Login Page', () => {
+  beforeEach(() => {
+    LoginForm.mockClear()
+  })
+
+  it('renders the login page', () => {
+    render(<Login />)
+    expect(screen.getByTestId('mocked-login-form')).toBeInTheDocument()
+  })
+
+  it('renders the LoginForm component', () => {
+    render(<Login />)
+    expect(LoginForm).toHaveBeenCalled()
+  })
+
+  it('LoginForm is rendered exactly once', () => {
+    render(<Login />)
+    expect(LoginForm).toHaveBeenCalledTimes(1)
+  })
+
+  it('matches snapshot', () => {
+    const { container } = render(<Login />)
+    expect(container).toMatchSnapshot()
+  })
+})

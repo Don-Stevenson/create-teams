8b10dad617c0324ace4b7186ef3b38c69f14a7a7
Don Stevenson <dstevenson@tribalscale.com>
2025-04-23 15:37:18 -0400

    Rendering initial

--- src/app/components/PlayerList.js
+++ src/app/components/PlayerList.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import PlayerCard from './PlayerCard'
+import LetterIndex from './LetterIndex'
 
 const PlayerList = ({ players, onEditPlayer, onDeletePlayer }) => {
+  const shownInitials = useRef(new Set())
+
   return (
     <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-2 p-2">
       {players.map(player => (
         <div key={player._id}>
+          <LetterIndex
+            player={player}
+            shownInitials={shownInitials.current}
+            setShownInitials={initial => shownInitials.current.add(initial)}
+          />
           <PlayerCard
             player={player}
             onEditPlayer={onEditPlayer}

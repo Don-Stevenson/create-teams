d869a3bc843df353dd975bad27d327276b7ab8cc
Don Stevenson <dstevenson@tribalscale.com>
2025-04-23 15:54:38 -0400

    Fixing styles on Player list

--- src/app/components/PlayerList.js
+++ src/app/components/PlayerList.js
@@ -1,24 +1,34 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import PlayerCard from './PlayerCard'
-import LetterIndex from './LetterIndex'
 
 const PlayerList = ({ players, onEditPlayer, onDeletePlayer }) => {
-  const shownInitials = useRef(new Set())
+  const playersByInitial = players.reduce((acc, player) => {
+    const initial = player.name.charAt(0).toUpperCase()
+    if (!acc[initial]) {
+      acc[initial] = []
+    }
+    acc[initial].push(player)
+    return acc
+  }, {})
+
+  const sortedInitials = Object.keys(playersByInitial).sort()
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-2 p-2">
-      {players.map(player => (
-        <div key={player._id}>
-          <LetterIndex
-            player={player}
-            shownInitials={shownInitials.current}
-            setShownInitials={initial => shownInitials.current.add(initial)}
-          />
-          <PlayerCard
-            player={player}
-            onEditPlayer={onEditPlayer}
-            onDeletePlayer={onDeletePlayer}
-          />
+    <div className="flex flex-col gap-8">
+      {sortedInitials.map(initial => (
+        <div key={initial} className="flex flex-col gap-4">
+          <div className="text-2xl font-bold text-red-700 px-2">{initial}</div>
+
+          <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-2 px-2">
+            {playersByInitial[initial].map(player => (
+              <PlayerCard
+                key={player._id}
+                player={player}
+                onEditPlayer={onEditPlayer}
+                onDeletePlayer={onDeletePlayer}
+              />
+            ))}
+          </div>
         </div>
       ))}
     </div>

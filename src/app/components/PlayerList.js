4f9219263de3fbc68f9304c98d5111020faf8ab0
Don Stevenson <50342473+Don-Stevenson@users.noreply.github.com>
2025-04-23 18:12:23 -0400

    Add letter indexes for player names (#99)
    * Rendering initial
    
    * Fixing styles on Player list
    
    * Add letter indexes on Player List toggle component

--- src/app/components/PlayerList.js
+++ src/app/components/PlayerList.js
@@ -2,15 +2,32 @@ import React from 'react'
 import PlayerCard from './PlayerCard'
 
 const PlayerList = ({ players, onEditPlayer, onDeletePlayer }) => {
+  const playersByInitial = players.reduce((acc, player) => {
+    const initial = player.name.charAt(0).toUpperCase()
+    if (!acc[initial]) {
+      acc[initial] = []
+    }
+    acc[initial].push(player)
+    return acc
+  }, {})
+
+  const sortedInitials = Object.keys(playersByInitial).sort()
+
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-2 p-2">
-      {players.map(player => (
-        <div key={player._id}>
-          <PlayerCard
-            player={player}
-            onEditPlayer={onEditPlayer}
-            onDeletePlayer={onDeletePlayer}
-          />
+    <div className="flex flex-col gap-4">
+      {sortedInitials.map(initial => (
+        <div key={initial} className="flex flex-col gap-4">
+          <div className="text-2xl font-bold text-red-700 px-2 ">{initial}</div>
+          <div className="grid grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-2 px-2">
+            {playersByInitial[initial].map(player => (
+              <PlayerCard
+                key={player._id}
+                player={player}
+                onEditPlayer={onEditPlayer}
+                onDeletePlayer={onDeletePlayer}
+              />
+            ))}
+          </div>
         </div>
       ))}
     </div>

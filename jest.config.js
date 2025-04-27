dd499b0963e11971bc48a24a40b5efd6ece19f7f
Don Stevenson <50342473+Don-Stevenson@users.noreply.github.com>
2025-04-27 14:35:26 -0400

    Fix test act warning (#105)
    * Wrap tests in act, update jest config and setup
    
    * Update login for test, update jest setup to only supress jest warnings
    
    * Add proper error handling for create teams test

--- jest.config.js
+++ jest.config.js
@@ -11,10 +11,14 @@ const config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   roots: ['.'],
   testMatch: ['**/*.test.js'],
   testPathIgnorePatterns: ['/server/'],
+  // Configure the test environment to support React's concurrent mode
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
